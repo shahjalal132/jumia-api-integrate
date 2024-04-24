fe7cd1c99aa2af030dca941b205a284ba450a855
Shah jalal <rjshahjalal132@gmail.com>
2024-04-24 15:54:39 +0600

    implement fetch products from api function oop style

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ class ProductSync {
     private $spreadsheetID;
     private $sheetRange;
     private $credentialsPath = __DIR__ . '/credentials.json';
+    private $accessToken;
 
     public function __construct() {
         $this->client = new Google\Client();
@@ -18,13 +19,46 @@ class ProductSync {
         $this->service       = new Google_Service_Sheets( $this->client );
         $this->spreadsheetID = '1igZQ5L-FlY7FTzqMpxPOzbscWLYo15hLW5s9YHwPRD4';
         $this->sheetRange    = 'products!A:E';
+        $this->accessToken   = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJtU0VNMmNQX0h4UldDTl9Lcm1Hal9zVjRRZWt6eUU1VTVlR2drUk5SOElNIn0.eyJqdGkiOiIzOGJmMjE2OS0zODhiLTQxNjctOGUzZC0zZjRlOGYyOTI5YTYiLCJleHAiOjE3MTM5OTQ4ODYsIm5iZiI6MCwiaWF0IjoxNzEzOTUxNjg2LCJpc3MiOiJodHRwczovL3ZlbmRvci1hcGktc3RhZ2luZy5qdW1pYS5jb20vYXV0aC9yZWFsbXMvYWNsIiwic3ViIjoiYWIxM2U1YmMtMTE4Ni00MGU5LTg3MDEtN2IzMTlkNDM3ZmFlIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiNzQwYjlhNmQtMWY5MS00Njg3LTgyNTAtZTQ5YjAxNTlkZjQwIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiYmU4MDYwOWUtYTUzYi00ZmI5LTkxYzMtZDM3OGE1ZGU2MTc1IiwiYWNyIjoiMSIsInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJiZW9iaXAyMDIwK3N0Z3Rlc3RAZ21haWwuY29tIiwibG9jYWxlIjoiZW4iLCJlbWFpbCI6ImJlb2JpcDIwMjArc3RndGVzdEBnbWFpbC5jb20ifQ.EjP5a5Vw063HE-QW6zD0H3X3sQK7Uo4RCdLNnFve-bA773OiHab9CwPHamEQKCsuSN_qnRqzEeHqvrIrJeL6a-1Hn_UxLYLufaFhZJiA204q07fm7-yykmgtzlALuskpngsuCG_1ds80qNNj4XpVxHhNhfRQnbVJLNyyU5wHH3CQRaqQwO9kmnJs_RZDWjByBnZxBN-KRSTeluV5sG1Qb5oD43g7LxRRgHHpcSUVVSrhI0Ab8-a0Hyzm1r3OH-or4dz5XwAgvKBTcZSarV0G1Y0yQdV4_UZbCHkjOosGmICrXZPql8byVxznlRE3C0KOVKmMsXSy0SY71zCJvQbFjA';
     }
 
+    /**
+     * Fetches products from the Jumia API.
+     *
+     * @return string The JSON-encoded products data retrieved from the API.
+     */
     public function fetchProductsFromApi() {
-        // Your existing code to fetch products from the API
-        return 'Hello';
+        $curl = curl_init();
+
+        curl_setopt_array(
+            $curl,
+            array(
+                CURLOPT_URL            => 'https://vendor-api-staging.jumia.com/catalog/products',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING       => '',
+                CURLOPT_MAXREDIRS      => 10,
+                CURLOPT_TIMEOUT        => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST  => 'GET',
+                CURLOPT_HTTPHEADER     => array(
+                    'Authorization: Bearer ' . $this->accessToken,
+                    'Cookie: __cf_bm=5vqn_tlYJwoX74LqdRmj_Ii.zJgUksjaV_O6ewUqE5U-1713852921-1.0.1.1-pcINdhnibkJOgYhazL36Xm9j1xjtmB926xN4ZfQt2G1tXYC5LKLc1IImoGUJAtZnCgch2uulyojmT8kQMHwimw',
+                ),
+            )
+        );
+
+        $response = curl_exec( $curl );
+
+        curl_close( $curl );
+        return $response;
     }
 
+    /**
+     * Fetch product from google sheet
+     *
+     * @return array The JSON-encoded products data retrieved from the API.
+     */
     public function fetchProductsFromSheets() {
         $response = $this->service->spreadsheets_values->get( $this->spreadsheetID, $this->sheetRange );
         return $response->getValues();
@@ -75,5 +109,10 @@ class ProductSync {
     }
 }
 
-$productSync = new ProductSync();
-$productSync->synchronizeProducts();
+$productSync    = new ProductSync();
+$ProductFromApi = $productSync->fetchProductsFromApi();
+
+echo '<pre>';
+print_r( $ProductFromApi );
+die();
+
