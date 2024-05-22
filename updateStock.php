ef666e1cfadd4a74a8f6d7cdb908da39b99ef080
shahjalal132 <rjshahjalal132@gmail.com>
2024-05-22 18:02:45 +0600

    updateStock.php file updated with conditionally

--- updateStock.php
+++ updateStock.php
@@ -1,2 +1,27 @@
 <?php
 
+// require config file
+require_once 'config.php';
+
+// require main file
+require_once 'main.php';
+
+// Fetch enabled or disabled status from the database
+try {
+    $sql  = "SELECT control_value FROM controls WHERE control_key = 'stock_update'";
+    $stmt = $conn->prepare( $sql );
+    $stmt->execute();
+    $stockUpdateControl = $stmt->fetchColumn();
+} catch (PDOException $e) {
+    // Handle PDO exception
+    echo "Error: " . $e->getMessage();
+    exit; // Exit the script if an error occurs
+}
+
+// Conditionally update product stock based on the status
+if ( $stockUpdateControl === 'stock-enable' ) {
+    $productSync = new ProductSync();
+    $productSync->updateProductsStock();
+} else {
+    echo "Stock update is disabled. No action taken.";
+}
